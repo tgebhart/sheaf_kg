d2e54161ecde5d3a90369a991a1d243f26454c17
tom gebhart <gebharttom8@gmail.com>
2022-08-06 14:41:21 -0500

    updated to compute extension once for each triplet in the batch with unique source and target vertex

--- sheaf_kg/interactions/multisection_structured_embedding.py
+++ sheaf_kg/interactions/multisection_structured_embedding.py
@@ -36,23 +36,74 @@ class ExtensionInteraction(MultisectionStructuredEmbeddingInteraction):
             raise ValueError(f'training mask percentage must be between 0 and 1, got {training_mask_pct}')
         self.training_mask_pct = training_mask_pct
 
-    def _forward_harmonic_extension(self, boundary, r, interior, to_score):
-        # Since the relation_shape is more than length 1, the r value is given as a sequence
-        # of the representations defined there. You can use tuple unpacking to get them out
+    # def _forward_harmonic_extension(self, boundary, r, interior, to_score):
+    #     # Since the relation_shape is more than length 1, the r value is given as a sequence
+    #     # of the representations defined there. You can use tuple unpacking to get them out
+    #     r_h, r_t = r
+
+    #     # get unique indices across batch dimension
+    #     # a little hacky, but it doesn't appear possible to get indices from `self`
+    #     _, b_idxs = torch.unique(boundary, dim=0, return_inverse=True, sorted=False)
+    #     _, i_idxs = torch.unique(interior, dim=0, return_inverse=True, sorted=False)
+
+    #     edge_index = torch.cat([b_idxs.unsqueeze(0), i_idxs.unsqueeze(0)], dim=0)
+    #     restriction_maps = torch.cat([r_h, r_t], dim=1)
+
+    #     Lschur = the.Kron_reduction(edge_index, restriction_maps, b_idxs, i_idxs)
+    #     costs = the.compute_costs(Lschur, b_idxs, i_idxs, boundary.reshape(Lschur.shape[0], -1), to_score, boundary.shape[-2] )
+    #     costs = costs.repeat((b_idxs.shape[0], 1))
+    #     return costs
+
+    def _forward_harmonic_extension(self, sources, r, targets, to_score):
+        # broadly, we wish to compute scores via harmonic extension for each 
+        # triplet in the incoming batch. for each triplet i, our approach will 
+        # be to treat the full batch as a subgraph and take the schur complement 
+        # of all of the batch entities that are not the head or tail entities of i. 
+
+        # we could alter this to take multiple source vertices.
+        # the sampling for such an approach seems arbitrary without a 
+        # particular complex query structure in mind.
+
+        # unpack restriction maps
         r_h, r_t = r
 
         # get unique indices across batch dimension
         # a little hacky, but it doesn't appear possible to get indices from `self`
-        _, b_idxs = torch.unique(boundary, dim=0, return_inverse=True, sorted=False)
-        _, i_idxs = torch.unique(interior, dim=0, return_inverse=True, sorted=False)
+        _, s_idxs = torch.unique(sources, dim=0, return_inverse=True, sorted=False)
+        _, t_idxs = torch.unique(targets, dim=0, return_inverse=True, sorted=False)
 
-        edge_index = torch.cat([b_idxs.unsqueeze(0), i_idxs.unsqueeze(0)], dim=0)
+        # edge_index is (2,ne)
+        edge_index = torch.cat([s_idxs.unsqueeze(0), t_idxs.unsqueeze(0)], dim=0)
+        # restriction maps is (ne,2,de,dv)
         restriction_maps = torch.cat([r_h, r_t], dim=1)
+        
+        # store scores for each triplet
+        scores = torch.empty((sources.shape[0], to_score.shape[1]))
 
-        Lschur = the.Kron_reduction(edge_index, restriction_maps, b_idxs, i_idxs)
-        costs = the.compute_costs(Lschur, b_idxs, i_idxs, boundary.reshape(Lschur.shape[0], -1), to_score, boundary.shape[-2] )
-        costs = costs.repeat((b_idxs.shape[0], 1))
-        return costs
+        for i in range(sources.shape[0]):
+            # source vertex representation
+            sv = sources[i]
+            # boundary indices
+            b_idxs = edge_index[:,i]
+            # interior indices which are not equal to either of the boundary indices
+            i_idxs = edge_index[:,(edge_index[0,:] != b_idxs[0]) & (edge_index[1,:] != b_idxs[1])]
+            # restriction maps of the above relations 
+            i_rmaps = restriction_maps[(edge_index[0,:] != b_idxs[0]) & (edge_index[1,:] != b_idxs[1]),:,:,:]
+
+            # add the head and tail entities of batch triplet i
+            filtered_edge_index = torch.cat([b_idxs.unsqueeze(1), i_idxs], dim=1)
+            # add the restriction maps for triplet i
+            filtered_restriction_maps = torch.cat([restriction_maps[i].unsqueeze(0), i_rmaps], dim=0)
+
+            # take the schur complement over the interior vertices
+            Lschur = the.Kron_reduction(filtered_edge_index, filtered_restriction_maps, b_idxs, i_idxs.flatten())
+            # reshape the entities that should be scored so they are sized (num_targets*dv, num_entities_to_score)
+            xT = torch.transpose(to_score.squeeze(0).squeeze(-1), 0,1)
+            # score all the entities over our schur complement Laplacian
+            # assign the source vertices to index 0 and the target vertices to index 1 w.r.t. the ordering of the cochain
+            costs = the.compute_costs(Lschur, torch.zeros(1), torch.ones(1), sv.flatten(), xT, sv.shape[-2] )
+            scores[i] = -costs
+        return scores
 
     def _masked_harmonic_extension(self, h,r,t):
         # TODO
