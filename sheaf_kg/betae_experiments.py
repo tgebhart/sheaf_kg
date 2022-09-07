c443ab9921132a113f4c614c04301b2c201f3d14
tgebhar <gebharttom8@gmail.com>
2022-09-07 08:03:21 -0500

    before pull

--- sheaf_kg/betae_experiments.py
+++ sheaf_kg/betae_experiments.py
@@ -1,14 +1,18 @@
+import pickle
 import itertools
 from sheaf_kg.betae_training_pipeline import run
 
 num_epochs = 250
 random_seed = 134
-datasets = ['FB15k-237', 'NELL']
-models = ['BetaeExtensionStructuredEmbedding', 'BetaeExtensionTransE']
+datasets = ['FB15k-237']
+models = ['BetaeExtensionTranslational']
 cochain_dims = [[32,32], [32,16], [32,8]]
 sections = [1, 16, 32]
 reg_weights = [0, 1e-2, 1e-1, 1]
 parametrizations = [None]
+
+training_batch_size = 1024
+evaluation_batch_size = 1
 
 def filter_irrelevant(x):
   if x[3] == 1 and x[4] > 0:
@@ -20,7 +24,8 @@ experiments = itertools.product(models, datasets, cochain_dims, sections, reg_we
 # filter out experiments from product which do not make sense
 experiments = list(filter(filter_irrelevant, experiments))
 
-for i in range(len(experiments)):
+skipped = []
+for i in range(1,len(experiments)):
     e = experiments[i]
     print(f'running experiment {i}/{len(experiments)}', e)
     model = e[0]
@@ -30,6 +35,15 @@ for i in range(len(experiments)):
     num_sections = e[3]
     reg_weight = e[4]
     parametrization = e[5]
-    run(model, dataset, num_epochs, random_seed,
+    try:
+        run(model, dataset, num_epochs, random_seed,
         embedding_dim, c1_dimension=c1_dimension, num_sections=num_sections, reg_weight=reg_weight,
         parameterization=parametrization)
+    except RuntimeError:
+        print('skipping', e)
+        skipped.append(e)
+        continue
+
+print('skipped', skipped)
+with open('data/skipped.pkl', 'wb') as f:
+    pickle.dump(skipped, f)
